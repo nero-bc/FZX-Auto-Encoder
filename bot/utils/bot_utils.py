2ad2dcc6b43f4ba37a4711086ab3b11f69f8662d
Salxchange <138751618+Salxchange@users.noreply.github.com>
2024-02-07 04:13:55 +0100

    Update bot_utils.py

--- bot/utils/bot_utils.py
+++ bot/utils/bot_utils.py
@@ -54,8 +54,8 @@ class Var_list:
     QUEUE = {}
     OK = {}
 
-    FINISHED_PROGRESS_STR = "🧡"
-    UN_FINISHED_PROGRESS_STR = "🤍"
+    FINISHED_PROGRESS_STR = "█"
+    UN_FINISHED_PROGRESS_STR = "▒"
     MAX_MESSAGE_LENGTH = 4096
 
 
